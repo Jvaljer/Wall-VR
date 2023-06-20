887d428577fa86171efa6489f6b9119030c44c83
Jvaljer <abbel.haol@gmail.com>
2023-06-20 11:10:22 +0200

    tryna debug Smarties

--- Project/Assets/SmartiesManager.cs
+++ Project/Assets/SmartiesManager.cs
@@ -20,7 +20,7 @@ public class SmartiesManager :  MonoBehaviour {
 
 	List<SmartiesWidget> expeWidgets;
 	List<SmartiesWidget> interfaceWidgets;
-	
+
 	public void StartFromOpe(Setup S_, Operator O_){
 		Debug.Log("Start SmartiesManager");
 		ope = O_;
@@ -39,17 +39,17 @@ public class SmartiesManager :  MonoBehaviour {
 			(int)wall.Width(), (int)wall.Height(), wall.ColumnsAmount(), wall.RowsAmount()
 		);
 	
-		/*
+	
 		//to add a widget : 
-			//first set up the grid
+		//first set up the grid
 		smarties.initWidgets(4,3);
 			//then initialize the container variable
 		SmartiesWidget wid;
 			//then add the wanted widget
-		wid = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_*, "text', x_pos, y_pos, x_size, y_size);
+		wid = smarties.addWidget(SmartiesWidget.SMARTIES_WIDGET_TYPE_BUTTON, "text", 1, 1, 1, 1);
 			//then some stuff I must check for utilities
 		wid.handler = letterHHandler; //might modify
-		wid_list.Add(wid); //optional ?
+		/* wid_list.Add(wid); //optional ?
 		*/
 
 		
@@ -108,6 +108,7 @@ public class SmartiesManager :  MonoBehaviour {
 		switch (e.type) {
 			case SmartiesEvent.SMARTIES_EVENTS_TYPE_CREATE:
 				Debug.Log("SM Create Puck: " + e.id);
+				Debug.Log("IH null : "+(input_handler==null));
 				input_handler.CreateMCursor(this, e.id, e.x, e.y, SmartiesColors.getPuckColorById(e.id));
 				break;
 
@@ -118,15 +119,18 @@ public class SmartiesManager :  MonoBehaviour {
 				break;
 
 			case SmartiesEvent.SMARTIES_EVENTS_TYPE_START_MOVE:
+				Debug.Log("SM -> Start Move");
 				input_handler.StartMoveMCursor(this, e.p.id, e.p.x, e.p.y,
 					(e.mode == SmartiesEvent.SMARTIES_GESTUREMOD_DRAG) );
 				break;
 
 			case SmartiesEvent.SMARTIES_EVENTS_TYPE_MOVE:
+				Debug.Log("SM -> Move");
 				input_handler.MoveMCursor(this, e.p.id, e.p.x, e.p.y);
 				break;
 
 			case SmartiesEvent.SMARTIES_EVENTS_TYPE_END_MOVE:
+				Debug.Log("SM -> End Move");
 				input_handler.StopMoveMCursor(this, e.p.id, e.p.x, e.p.y);
 				break;
 
@@ -139,5 +143,11 @@ public class SmartiesManager :  MonoBehaviour {
 			default:
 				break;
 		}
+	}
+
+	public bool letterHHandler(SmartiesWidget w, SmartiesEvent e, object user_data)
+	{
+		Debug.Log("letteHHandler: HHHHHH");
+		return true;
 	}
 }
